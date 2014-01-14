fc968d132a2ae594939b746ada3320fb21564b4e
Viblio <viblio@viblio-VB.(none)>
2014-01-14 11:55:09 -0800

    adding changes discussed on friday meeting 10 Jan 2014

--- viblio/projects/feature_extractor/vector_quantization.py
+++
@@ -1,8 +0,0 @@
-
-class vector_quantization():
-    def hard_assignment(self,descrs, centers):
-        pass
-    def soft_kernel_assignment_v2(self,descrs,centers,gamma):
-        pass
-    def create_spatial_pyramid(self,q_ftr,frames,maxPyramidLevel, imageSize, branchFact):
-        pass
