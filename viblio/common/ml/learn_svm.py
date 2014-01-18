2d7513f19ddb098e18481a1cf62106184edc5e3d
mrvahdat <vahdat.ce@gmail.com>
2014-01-17 20:01:06 -0800

    learn_svm is written in inheritance format

--- viblio/common/ml/learn_svm.py
+++ viblio/common/ml/learn_svm.py
@@ -1,28 +1,67 @@
 import numpy as np
 from sklearn import svm
+import pickle
 
 
-def compute_kernel(a, b):
-    kernel = np.zeros((a.shape[0], b.shape[0]), dtype='float')
-    for ind in range(a.shape[0]):
-        v = a[ind, :]
-        kernel[ind, :] = (0.5 * np.sum(v + b - np.abs(v - b), axis=1))
+class Kernel(object):
+    def __init__(self, param):
+        self.param = param
 
-    return kernel
-
-
-def train_svm(features, labels, C):
-    kernel = compute_kernel(features, features)
-    clf = svm.SVC(kernel='precomputed', C=C, probability=True, class_weight='auto', random_state=30)
-    clf.fit(kernel, labels)
-
-    return clf
+    def compute(self, a, b):
+        pass
 
 
-def test_svm(clf, train_features, test_feature):
-    kernel = compute_kernel(train_features, test_feature)
-    prob = clf.predict_proba(kernel)
-    return prob[:, clf._label==1]
+class HIK(Kernel):
+    def compute(self, a, b):
+        gram = np.zeros((a.shape[0], b.shape[0]), dtype='float')
+        for ind in range(a.shape[0]):
+            v = a[ind, :]
+            gram[ind, :] = (0.5 * np.sum(v + b - np.abs(v - b), axis=1))
+
+        return gram
+
+
+class SVMClassifier(object):
+    def __init__(self, C, kernel):
+        self.C = C
+        self.kernel = kernel
+
+    def learn(self, train_feature, labels):
+        pass
+
+    def project(self, train_feature, test_feature):
+        pass
+
+    def save(self, filename):
+        output = open(filename, 'wb')
+        pickle.dump(self, output)
+        output.close()
+
+    def load(self, filename):
+        pass
+
+
+class SKLearnSMV(SVMClassifier):
+    def __init__(self, C, kernel):
+        super(SKLearnSMV, self).__init__(C, kernel)
+        self.clf = svm.SVC(kernel='precomputed', C=self.C, probability=True, class_weight='auto', random_state=30)
+
+    def learn(self, features, labels):
+        kernel = self.kernel.compute(features, features)
+        self.clf.fit(kernel, labels)
+
+    def project(self, train_features, test_feature):
+        kernel = self.kernel.compute(train_features, test_feature)
+        prob = self.clf.predict_proba(kernel)
+        return prob[:, self.clf._label==1]
+
+    def load(self, filename):
+        inputfile = open(filename, 'rb')
+        temp = pickle.load(inputfile)
+        self.C = temp.C
+        self.kernel = temp.kernel
+        self.clf = temp.clf
+        inputfile.close()
 
 
 
