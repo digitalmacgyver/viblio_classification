d487fbe9ef9cd2d00c74da9b1ea36322abb25050
Ramsri Golla <rgolla@ubuntu.(none)>
2014-01-18 07:18:50 -0800

    update to avoid crash bcoz of newline at end of txt file

--- viblio/projects/viblio_classification/feature_extractor.py
+++ viblio/projects/viblio_classification/feature_extractor.py
@@ -1,5 +1,7 @@
 
 
+import sys
+sys.path.extend(['/home/rgolla/Desktop/classification'])
 
 import argparse
 import os
@@ -40,48 +42,49 @@ if __name__ == '__main__':
 
     # Loop through each url and extract feature
     for index,line in enumerate(content):
-        fd = urllib.urlopen(line.split()[1])
-        image_file = cStringIO.StringIO(fd.read())
-        im = Image.open(image_file)
-        basewidth = 640
-        wpercent = (basewidth/float(im.size[0]))
-        hsize = int((float(im.size[1])*float(wpercent)))
-        im = im.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+        if(index<len(content)-1):
+            fd = urllib.urlopen(line.split()[1])
+            image_file = cStringIO.StringIO(fd.read())
+            im = Image.open(image_file)
+            basewidth = 640
+            wpercent = (basewidth/float(im.size[0]))
+            hsize = int((float(im.size[1])*float(wpercent)))
+            im = im.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
         #print im.size
-	print str(index)+'/'+str(len(content)-1)
-        pix = numpy.array(im)
+            print str(index)+'/'+str(len(content)-1)
+            pix = numpy.array(im)
         # extract hog 2d descriptors
-        hog2D_detector = features.Hog2x2FeatureDetector()
-        hog2D_descriptor = features.Hog2x2FeatureDescriptor()
-        raw_feature = hog2D_descriptor.extract(pix)
-        floc, fdesc = hog2D_descriptor.run(pix)
+            hog2D_detector = features.Hog2x2FeatureDetector()
+            hog2D_descriptor = features.Hog2x2FeatureDescriptor()
+            raw_feature = hog2D_descriptor.extract(pix)
+            floc, fdesc = hog2D_descriptor.run(pix)
 
         # extract codebooks
-        codebook_file = config.resource_dir() + '/features/codebooks/' + hog2D_descriptor.params['codebook_file']
-        codebook_mat = scipy.io.loadmat(codebook_file)
+            codebook_file = config.resource_dir() + '/features/codebooks/' + hog2D_descriptor.params['codebook_file']
+            codebook_mat = scipy.io.loadmat(codebook_file)
 
 
         # quantize feature
-        q_params = dict()
-        q_params['kNN'] = int(hog2D_descriptor.params['kNN'])
-        q_params['gamma'] = float(hog2D_descriptor.params['gamma'])
-        q_params['whiten'] = int(hog2D_descriptor.params['whiten'])
-        vq = feature_pooling.SoftKernelQuantization(codebook_file, q_params)
-        quantized_ftr = vq.project(fdesc)
+            q_params = dict()
+            q_params['kNN'] = int(hog2D_descriptor.params['kNN'])
+            q_params['gamma'] = float(hog2D_descriptor.params['gamma'])
+            q_params['whiten'] = int(hog2D_descriptor.params['whiten'])
+            vq = feature_pooling.SoftKernelQuantization(codebook_file, q_params)
+            quantized_ftr = vq.project(fdesc)
 
         # create spatial pyramid
-        max_level = int(hog2D_descriptor.params['maxPyramidLevel'])
-        branch_factor = int(hog2D_descriptor.params['branchFact'])
-        sp_pyramid = feature_pooling.SpatialPyramid(max_level, branch_factor)
-        (h, w, nc) = pix.shape
-        spatial_ftr = sp_pyramid.create(quantized_ftr, floc, (h, w))
+            max_level = int(hog2D_descriptor.params['maxPyramidLevel'])
+            branch_factor = int(hog2D_descriptor.params['branchFact'])
+            sp_pyramid = feature_pooling.SpatialPyramid(max_level, branch_factor)
+            (h, w, nc) = pix.shape
+            spatial_ftr = sp_pyramid.create(quantized_ftr, floc, (h, w))
         
-        x = numpy.vstack([x,spatial_ftr])
-	if int(line.split()[3])==0:
-		label=-1
-	else:
-		label=1
-	labels=numpy.vstack([labels,label])
+            x = numpy.vstack([x,spatial_ftr])
+            if int(line.split()[3])==0:
+                label=-1
+            else:
+                label=1
+                labels=numpy.vstack([labels,label])
 
         #print spatial_ftr.shape
 
