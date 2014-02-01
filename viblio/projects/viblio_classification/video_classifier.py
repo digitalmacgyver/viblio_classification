c4ad33a1710be2e8100dc5ed9229824da59a5a8a
Ramsri Golla <rgolla@ubuntu.(none)>
2014-01-31 18:26:39 -0800

    Modification to video_classifier

--- viblio/projects/viblio_classification/video_classifier.py
+++ viblio/projects/viblio_classification/video_classifier.py
@@ -4,13 +4,14 @@ import argparse
 
 from viblio.common.utils import videoutils
 
-#usage :  python video_classifier.py -v /home/rgolla/Desktop/vid4.mp4 -d /home/rgolla/Downloads/vid4 -config /home/rgolla/Desktop/classification/viblio/resources/ml/svm_config.cfg
+#usage :  python video_classifier.py -v /home/rgolla/Desktop/vid4.mp4 -d /home/rgolla/Downloads/vid4 -config /home/rgolla/Desktop/classification/viblio/resources/ml/svm_config.cfg -m /home/rgolla/Desktop/svm_default.model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action='store', dest='video_file', help='Video File that neeeds to be classified')
     parser.add_argument('-d', action='store', dest='output_directory', help='directory to store the results')
     parser.add_argument('-config', action='store', dest='config_file', help='directory to store the results')
+    parser.add_argument('-m', action='store', dest='model_file', help='directory to store the results')
     results = parser.parse_args()
 
     if not os.path.exists(results.output_directory):
@@ -23,7 +24,7 @@ if __name__ == '__main__':
     #extract features from the frames of the video
     os.system('python feature_extractor.py -i %s -o %s -inter_dir %s'%(text_path,path1,results.output_directory))
     #classify the extracted frames from the video
-    os.system('python viblio_classifier.py -d %s -i %s_features.txt -c %s -s predict'%(results.output_directory,path1,results.config_file))
+    os.system('python viblio_classifier.py -d %s -i %s_features.txt -c %s -s predict -m %s'%(results.output_directory,path1,results.config_file,results.model_file))
     print "starting actual prediction of frames"
     with open('/home/rgolla/Downloads/%s/prediction.txt'%path1) as f:
         content=f.readlines()
