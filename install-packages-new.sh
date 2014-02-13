8974e5aefd70fdddf547ba17011c306b6e859d0f
root <root@ip-10-254-45-109.us-west-2.compute.internal>
2014-02-13 19:47:34 +0000

    Adding change to install script to check opencv version

--- install-packages-new.sh
+++ install-packages-new.sh
@@ -1,36 +1,72 @@
-#Installing Opencv 2.4.5
 
-arch=$(uname -m)
-if [ "$arch" == "i686" -o "$arch" == "i386" -o "$arch" == "i486" -o "$arch" == "i586" ]; then
-flag=1
+function opencv_install(){
+    
+				    arch=$(uname -m)
+				    if [ "$arch" == "i686" -o "$arch" == "i386" -o "$arch" == "i486" -o "$arch" == "i586" ]; then
+				    flag=1
+				    else
+				    flag=0
+				    fi
+				    echo "Installing OpenCV 2.4.5"
+				    mkdir OpenCV
+				    cd OpenCV
+				    echo "Removing any pre-installed ffmpeg and x264"
+				    sudo apt-get -y remove ffmpeg x264 libx264-dev
+				    echo "Installing Dependenices"
+				    sudo apt-get -y install libopencv-dev
+				    sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
+				    sudo apt-get -y install libtiff4-dev libjpeg-dev libjasper-dev
+				    sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
+				    sudo apt-get -y install python-dev python-numpy
+				    sudo apt-get -y install libtbb-dev
+				    sudo apt-get -y install libqt4-dev libgtk2.0-dev
+				    sudo apt-get -y install libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
+				    sudo apt-get -y install x264 v4l-utils ffmpeg
+				    echo "Downloading OpenCV 2.4.5"
+				    wget -O OpenCV-2.4.5.tar.gz http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz/download
+				    echo "Installing OpenCV 2.4.5"
+				    tar -xvf OpenCV-2.4.5.tar.gz
+				    cd opencv-2.4.5
+				    mkdir build
+				    cd build
+				    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+				    make -j4
+				    sudo make install
+				    sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+				    sudo ldconfig
+				    echo "OpenCV 2.4.5 ready to be used"
+
+}
+
+
+
+opencv_version=`pkg-config --modversion opencv`
+echo $opencv_version
+check_version="2.4.5"
+echo $check_version
+
+if test "$opencv_version" = "$check_version"
+then
+   echo "version is 2.4.5"
 else
-flag=0
+   echo "current version is not 2.4.5."
+   echo "Do you wish to install opencv 2.4.5?"
+   select yn in "Yes" "No"
+   do
+      case $yn in
+         Yes ) 
+	     echo "calling opencv install";
+	     opencv_install
+	     break;;
+         No ) 
+	     echo "terminating";
+	     exit;
+	     break;;
+      esac
+   done
+
 fi
-echo "Installing OpenCV 2.4.5"
-mkdir OpenCV
-cd OpenCV
-echo "Removing any pre-installed ffmpeg and x264"
-sudo apt-get -y remove ffmpeg x264 libx264-dev
-echo "Installing Dependenices"
-sudo apt-get -y install libopencv-dev
-sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
-sudo apt-get -y install libtiff4-dev libjpeg-dev libjasper-dev
-sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
-sudo apt-get -y install python-dev python-numpy
-sudo apt-get -y install libtbb-dev
-sudo apt-get -y install libqt4-dev libgtk2.0-dev
-sudo apt-get -y install libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
-sudo apt-get -y install x264 v4l-utils ffmpeg
-echo "Downloading OpenCV 2.4.5"
-wget -O OpenCV-2.4.5.tar.gz http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz/download
-echo "Installing OpenCV 2.4.5"
-tar -xvf OpenCV-2.4.5.tar.gz
-cd opencv-2.4.5
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j4
-sudo make install
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-sudo ldconfig
-echo "OpenCV 2.4.5 ready to be used"
+
+
+
+
