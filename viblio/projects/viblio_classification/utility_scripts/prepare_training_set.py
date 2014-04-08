6cf003a29dd27e3f5788c80d028899bb6c7801f1
Ramsri Golla <rgolla@viblio.com>
2014-04-08 12:43:11 -0700

    minor change

--- viblio/projects/viblio_classification/utility_scripts/prepare_training_set.py
+++ viblio/projects/viblio_classification/utility_scripts/prepare_training_set.py
@@ -25,7 +25,7 @@ negatives=['bicycle','birthday','classroom','playground','skiing','water']
 
 for each1 in positives:
     files1=filter(lambda x:x.endswith('.hdf'),sorted(os.listdir(each1)))
-    filtered1=files1[0:results.pos_ftrs]
+    filtered1=files1[0:int(results.pos_ftrs)]
     print len(filtered1),each1
     for each_line1 in filtered1:
         training_data.write('%s %s %d\n'%(each1,each1+'/'+each_line1,1))
@@ -36,7 +36,7 @@ for each1 in positives:
 for each in negatives:
     files=filter(lambda x:x.endswith('.hdf'),sorted(os.listdir(each)))
 
-    filtered=files[0:results.neg_ftrs]
+    filtered=files[0:int(results.neg_ftrs)]
 
     for each_line in filtered:
         training_data.write('%s %s %d\n'%(each,each+'/'+each_line,-1))
